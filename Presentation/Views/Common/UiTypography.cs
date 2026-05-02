72b79a3606cc60c353b429e64efd585822b20a10
Bakheet <bakhe8@users.noreply.github.com>
2026-05-02 05:45:36 +0300

    Use IBM Plex Sans Arabic font

--- Presentation/Views/Common/UiTypography.cs
+++ Presentation/Views/Common/UiTypography.cs
@@ -5,7 +5,9 @@ namespace GuaranteeManager
 {
     public static class UiTypography
     {
-        public const string DefaultFontFamilyName = "Segoe UI Variable Text, Segoe UI, Tahoma";
+        public const string DefaultFontFamilyName = "IBM Plex Sans Arabic";
+        public const string EmbeddedFontFamilyPath = "./Assets/Fonts/IBMPlexSansArabic/#IBM Plex Sans Arabic";
+        public const string FallbackFontFamilyName = "Segoe UI Variable Text, Segoe UI, Tahoma";
         public const double Tiny = 9d;
         public const double Small = 10d;
         public const double Secondary = 11d;
@@ -16,7 +18,9 @@ namespace GuaranteeManager
         public const double Kpi = 32d;
         public const double HeroNumber = 34d;
 
-        public static FontFamily DefaultFontFamily { get; } = new(DefaultFontFamilyName);
+        public static FontFamily DefaultFontFamily { get; } = new(
+            new Uri("pack://application:,,,/"),
+            EmbeddedFontFamilyPath);
 
         public static FontWeight RegularWeight => FontWeights.Normal;
         public static FontWeight EmphasisWeight => FontWeights.SemiBold;
